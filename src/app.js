affa2a3be64b5275be64ab4a9088951432da2ca7
Alejandro Huertas <ahuertas@fi.uba.ar>
2023-10-17 20:48:00 -0300

    feature add views hbs

--- src/app.js
+++ src/app.js
@@ -1,22 +1,30 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
-
+import handlebars from 'express-handlebars'
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+import viewRouter from './routes/view.router.js'
+import __dirname from './utils.js';
 
 const app = express();
-const PORT = process.env.PORT||9090;
+const PORT = process.env.PORT || 9090;
 const connection = mongoose.connect(process.env.MONGO_URL)
+
+// confi de hbs
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/users',usersRouter);
-app.use('/api/pets',petsRouter);
-app.use('/api/adoptions',adoptionsRouter);
-app.use('/api/sessions',sessionsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/pets', petsRouter);
+app.use('/api/adoptions', adoptionsRouter);
+app.use('/api/sessions', sessionsRouter);
+app.use('/', viewRouter)
 
-app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
