83e9577039e734b364e45e6dd002304541238a84
Alejandro Huertas <ahuertas@fi.uba.ar>
2024-06-11 20:46:47 -0300

    se agrego vista de userTest

--- src/routes/view.router.js
+++ src/routes/view.router.js
@@ -5,4 +5,15 @@ router.get("/", (req, res) => {
     res.render('index', {})
 })
 
+
+router.get("/test-user", (req, res) => {
+    let testUser = {
+        first_name: "Hilda",
+        last_name: "Martinez",
+        email: 'hilda@gmail.com'
+    }
+    res.render("users", { user: testUser })
+})
+
+
 export default router
